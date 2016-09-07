6c289991759d50fc136e86385e9cee322c1aaa9f
George Rogers <grogers385@gmail.com>
2016-09-07 11:55:17 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
-pub mod stors;
+extern crate iron;
+extern crate router;
+extern crate staticfile;
+extern crate handlebars_iron;
 
-pub fn main() {
+use iron::prelude::*;
+
+pub mod handlers;
+
+pub fn main(on: &str) {
+    let f = staticfile::Static::new("public");
+    let mut r = router::Router::new();
+    r.get("/", handlers::index, "index");
+    r.get("/article/:slug", handlers::article, "article");
+    r.get("/static/*", f, "static");
+    Iron::new(r).http(on).unwrap();
+    println!("Listening on: {}", on);
 }
