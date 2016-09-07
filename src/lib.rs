eb5ed4829f28cbf03b424b34bd67863fe89cc7b9
George Rogers <grogers385@gmail.com>
2016-09-07 15:37:52 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate iron;
+extern crate mount;
 extern crate router;
 extern crate staticfile;
 extern crate rustc_serialize;
@@ -13,14 +14,16 @@ pub mod model;
 pub mod handlers;
 
 pub fn main(on: &str) {
-    let f = staticfile::Static::new("public");
-    let mut r = router::Router::new();
     let articles = model::load_articles(glob("articles/*.html").unwrap()).unwrap();
     let article_map = model::articles_map(&articles);
+    let mut m = mount::Mount::new();
+    let mut r = router::Router::new();
     r.get("/", move |r: &mut Request| handlers::index(&articles, r), "index");
     r.get("/article/:slug", move |r: &mut Request| handlers::article(&article_map, r), "article");
-    r.get("/static/*", f, "static");
-    let mut x = iron::Chain::new(r);
+    m.mount("/", r);
+    let f = staticfile::Static::new("public");
+    m.mount("/static/", f);
+    let mut x = iron::Chain::new(m);
     let mut hbs = HandlebarsEngine::new();
     hbs.add(Box::new(DirectorySource::new("templates/", ".hbs")));
     hbs.reload().unwrap();
