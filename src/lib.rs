78d49cea9eb004bc5c61f13b15d7a9f561697783
George Rogers <grogers385@gmail.com>
2016-09-07 13:33:59 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
 extern crate iron;
 extern crate router;
 extern crate staticfile;
+extern crate rustc_serialize;
 extern crate handlebars_iron;
+extern crate glob;
 
+use glob::glob;
 use iron::prelude::*;
-use handlebars_iron::HandlebarsEngine;
+use handlebars_iron::{HandlebarsEngine, DirectorySource};
 
 pub mod handlers;
 
 pub fn main(on: &str) {
     let f = staticfile::Static::new("public");
     let mut r = router::Router::new();
-    r.get("/", handlers::index, "index");
-    r.get("/article/:slug", handlers::article, "article");
+    let articles = handlers::load_articles(glob("articles/*.html").unwrap()).unwrap();
+    let article_map = handlers::articles_map(&articles);
+    r.get("/", move |r: &mut Request| handlers::index(&articles, r), "index");
+    r.get("/article/:slug", move |r: &mut Request| handlers::article(&article_map, r), "article");
     r.get("/static/*", f, "static");
     let mut x = iron::Chain::new(r);
     let mut hbs = HandlebarsEngine::new();
